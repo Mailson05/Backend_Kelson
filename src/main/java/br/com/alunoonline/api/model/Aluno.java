e26456ae8a150687ad01b8617c3ef23b0401430a
José Mailson Marques da Silva <2024111510165@academicos.com>
2025-03-11 20:36:31 -0300

    Base

---
+++ src/main/java/br/com/alunoonline/api/model/Aluno.java
@@ -0,0 +1,21 @@
+package br.com.alunoonline.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor // construtores sem argumentos
+@AllArgsConstructor // construtores com argumentos
+@Data //ja faz get e setter
+@Table(name = "aluno") //criar tabela no banco de dados
+@Entity //Informar que e uma entidade
+public class Aluno {
+    private String nome;
+    private String cpf;
+    private String email;
+    @Id // identificando como chave primario
+    @GeneratedValue(strategy = GenerationType.AUTO) // no pessoal usar identity
+    private Long id;
+
+}
