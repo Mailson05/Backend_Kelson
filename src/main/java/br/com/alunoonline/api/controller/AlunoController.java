fa2c5a7378120415b293232d62df7c0f74d89b1a
Mailson <mailsonestudo48@gmail.com>
2025-03-25 19:42:27 -0300

    atu

--- src/main/java/br/com/alunoonline/api/controller/AlunoController.java
+++ src/main/java/br/com/alunoonline/api/controller/AlunoController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-@RestController // significa boas praticas
+@RestController // significa boas praticas anotação
 @RequestMapping("/alunos") //path = caminho é em plural
 public class AlunoController {
 
