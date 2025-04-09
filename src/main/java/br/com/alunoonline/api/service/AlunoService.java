ccc23e0e612961e8d0082b22acdfb9b84acf3892
José Mailson Marques da Silva <2024111510165@academicos.com>
2025-04-08 21:11:51 -0300

    atualizandoAula08_04

--- src/main/java/br/com/alunoonline/api/service/AlunoService.java
+++ src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -5,16 +5,25 @@ import br.com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class AlunoService {
+public class AlunoService { // ele que tem a regra de negocio
 
     @Autowired
-    AlunoRepository alunoRepository; // ele injetar as dependencias da coisa
+    AlunoRepository alunoRepository; // ele injetar as dependencias, Repository que tem comunicação com Banco de dados
 
     public void criarAluno(Aluno aluno){
         alunoRepository.save(aluno);
 
     }
 
+    public List<Aluno> listarTodosAlunos(){
+        return alunoRepository.findAll(); // fazendo com que retorne todos alunos atarves do repository
+    }
 
+    public Optional<Aluno> buscarAlunoPorId(Long id){
+        return alunoRepository.findById(id);
+    }
 }
